3d1519d6161879d55ab7d93453c691306650d141
KrouClar <diprimot@gmail.com>
2026-08-16 20:14:33 +0200

    Update and rename container_portainer to container_portainer.sh

--- container_portainer.sh
+++ container_portainer.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "Creating the volume for Portainer..."
+echo "==========Creating the volume for Portainer=========="
 docker volume create portainer_data
 
-echo "Download and launch of the Portainer container..."
+echo "==========Download and launch of the Portainer container...=========="
 docker run -d -p 8000:8000 -p 9443:9443 --name portainer \
     --restart=always \
     -v /var/run/docker.sock:/var/run/docker.sock \

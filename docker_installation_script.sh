13b1b83e96db9cd73c67bc9b9646ed1ecc2b0479
KrouClar <diprimot@gmail.com>
2026-08-16 20:13:26 +0200

    Add echo statements for installation steps

--- docker_installation_script.sh
+++ docker_installation_script.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Uninstall packages 
+echo "Uninstall packages" 
 sudo apt remove $(dpkg --get-selections docker.io docker-compose docker-doc docker-buildx podman-docker containerd runc | cut -f1)
 
-# Add Docker's official GPG key:
+echo "Add Docker's official GPG key"
 sudo apt update
 sudo apt install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add the repository to Apt sources:
+echo "Add the repository to Apt sources"
 sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
 Types: deb
 URIs: https://download.docker.com/linux/debian
@@ -22,8 +22,8 @@ EOF
 
 sudo apt update
 
-# Installing lastest version of Docker
+echo "Installing lastest version of Docker"
 sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# Verify installation
+echo "Verify installation"
 sudo docker run hello-world
